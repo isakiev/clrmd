5999e20534131730dc69e3a834135e86d9d91192
Ilya Usov <Ilya.Usov@jetbrains.com>
2018-08-02 13:15:55 +0300

    Add Utils and Annotations

---
+++ src/Microsoft.Diagnostics.Runtime/src/Utilities/FuncUtil.cs
@@ -0,0 +1,14 @@
+using System;
+using JetBrains.Annotations;
+
+namespace Microsoft.Diagnostics.Runtime.Utilities
+{
+    internal static class FuncUtil<T>
+    {
+        /// <summary>
+        /// Identity function that always returns the same value that was used as its argument.
+        /// </summary>
+        [NotNull]
+        public static readonly Func<T, T> Identity = t => t;
+    }
+}
