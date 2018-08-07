0be7fc28bc74dce7bf4567a3733014d5fa3757bf
Ilya Usov <Ilya.Usov@jetbrains.com>
2018-08-07 20:30:20 +0300

    Create DataReaderUtil and ModuleUtil
    Add GetVersions to Public API
    
    (cherry picked from commit 9823ac89b5aa2be1c62a9e5ecd3782e7b0695825)

---
+++ src/Microsoft.Diagnostics.Runtime/src/Utilities/ModuleUtil.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Diagnostics.Runtime.Utilities
+{
+    public static class ModuleUtil
+    {
+        public static readonly Regex InvalidChars = new Regex($"[{Regex.Escape(new string(Path.GetInvalidPathChars()))}]");
+
+        public static IReadOnlyCollection<ClrInfo> GetVersions([NotNull] this IEnumerable<ModuleInfo> modules, Architecture architecture, out bool hasNativeRuntimes)
+        {
+            var versions = new List<ClrInfo>();
+            hasNativeRuntimes = false;
+
+            foreach (var module in modules)
+            {
+                if (ClrInfoProvider.IsSupportedRuntime(module, out var flavor))
+                    versions.Add(new ClrInfo(flavor, module, architecture));
+                else if (ClrInfoProvider.IsNativeRuntime(module))
+                    hasNativeRuntimes = true;
+            }
+
+            return versions;
+        }
+    }
+}
