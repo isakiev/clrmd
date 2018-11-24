f81a70a4a1cd3f1b058c4d08bcb30a6c5ae88325
Ruslan Isakiev <ruslan.isakiev@gmail.com>
2018-11-24 18:55:57 +0300

    migrate 0db14df48b6e73925db64a91c0ae148c8613ff00, 820afbbfe0f7341e0fc91c934477d56c51cbddc2 fixed docs

--- Src/Microsoft.Diagnostics.Runtime/Src/Common/Types/ClrException.cs
+++ Src/Microsoft.Diagnostics.Runtime/Src/Common/Types/ClrException.cs
@@ -4,13 +4,13 @@ namespace Microsoft.Diagnostics.Runtime
 {
   /// <summary>
   ///   A wrapper class for exception objects which help with common tasks for exception objects.
-  ///   Create this using GCHeap.GetExceptionObject.  You may call that when GCHeapType.IsException
+  ///   Create this using GCHeap.GetExceptionObject.  You may call that when ClrType.IsException
   ///   returns true.
   /// </summary>
   public abstract class ClrException
   {
     /// <summary>
-    ///   Returns the GCHeapType for this exception object.
+    ///   Returns the ClrType for this exception object.
     /// </summary>
     public abstract ClrType Type { get; }
 
